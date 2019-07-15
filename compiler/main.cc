a1aed22b8c94b160facdebb19cdea4792e27cda8
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2019-07-15 16:31:43 +0200

    compiler: parsed args and opened files

--- compiler/main.cc
+++ compiler/main.cc
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello world, compiler" << std::endl;
+void compile(const std::ifstream& words, std::ofstream& bin)
+{
+    bin << "test" << std::endl;
 }
+
+int main(int argc, char** argv)
+{
+    if (argc < 3)
+    {
+        std::cerr
+            << "Usage: ./ref/TextMiningCompiler /path/to/word/freq.txt /path/to/output/dict.bin"
+            << std::endl;
+        return 1;
+    }
+
+    std::cout << "Size structure: 4" << std::endl;
+
+    std::ifstream freq_file(argv[1], std::ifstream::in);
+    if (!freq_file.is_open())
+    {
+        std::cout << "File not found: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    std::ofstream bin_file(argv[2], std::ifstream::out);
+    if (!freq_file.is_open())
+    {
+        std::cout << "File not found: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    compile(freq_file, bin_file);
+
+    freq_file.close();
+    bin_file.close();
+
+    return 0;
+}
+
