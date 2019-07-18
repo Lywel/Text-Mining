88186f87652bb87b74a70616b69b9db8ea62e2c6
Martin Noel <martin.noel@epita.fr>
2019-07-18 18:31:38 +0200

    [levenstein] new implem

--- app/main.cc
+++ app/main.cc
@@ -3,33 +3,43 @@
 #include <boost/archive/binary_iarchive.hpp>
 #include "trie.hh"
 
+int mini(int a, int b, int c){
+    return(min(a, min(b,c)));
+}
+
 uint16_t levenstein_dist(const std::string& s1, const std::string& s2)
 {
-  size_t size1 = s1.size();
-  size_t size2 = s2.size();
-  size_t d[size1 + 1][size2 + 1];
-  for (int i = 0; i <= size1; i ++)
-    d[i][0] = i;
-  for (int i = 0; i <= size2; i ++)
-    d[0][i] = i;
+    int size1 = s1.size(), size2 = s2.size();
+    int suppr_dist, insert_dist, subs_dist, val;
+    int* dist = new int[(size1 + 1) * (size2 + 1)];
 
-  int cost = 0;
-  for (int i = 1; i <= size1; i ++)
-    for (int j = 1; j <= size2; j ++)
+    for (int i = 0; i < size1 + 1; ++i)
+        dist[(size2 + 1) * i] = i;
+
+    for (int j = 0; j < size2 + 1; ++j)
+        dist[j] = j;
+
+    for (int i = 1; i < size1 + 1; ++i)
     {
-      cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1 ;
-      if ( (i > 1) && (j > 1) && (s1[i] == s2[j - 1]) && (s1[i - 1] == s2[j]))
+      for (int j = 1; j < size2 + 1; ++j)
       {
-        size_t a = std::min(d[i - 1][j], d[i][j - 1] + 1);
-        size_t b = std::min(d[i][j] + cost, d[i - 2][j - 2]);
-        d[i][j] = std::min(a, b);
-      }
-      else
-      {
-        d[i][j] = std::min(std::min(d[i][j -1] + 1, d[i - 1][j] + 1), d[i - 1][j - 1] + cost);
+        suppr_dist = dist[(size2 + 1) * (i - 1) + j] + 1;
+        insert_dist = dist[(size2 + 1) * i + j - 1] + 1;
+        subs_dist = dist[(size2 + 1) * (i - 1) + j - 1];
+        
+        if (word1[i-1] != word2[j-1])
+          subs_dist += 1;
+        
+        val = mini(suppr_dist, insert_dist, subs_dist);
+        if(((i>=2) && (j>=2)) && ((word1[i-1]==word2[j-2]) && (word1[i-2]==word2[j-1])))
+              val = std::min(dist[(size2+1)*(i-2)+j-2]+1, val);
+          dist[(size2+1)*i+j] = val;
       }
     }
-  return d[size1][size2];
+
+    int res = dist[(size1+1)*(size2+1) - 1];
+    delete dist;
+    return(res);
 }
 
 void search_aux(trie_set& res, const TrieNode& cur, const std::string& prev, const std::string& str, uint16_t distance)
